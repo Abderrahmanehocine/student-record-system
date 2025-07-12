fc213a439a0ca327af76f22d0318c1db745c1bcc
Hocine Abderrahmane <hocineabderrahmane05@gmail.com>
2025-07-12 18:54:14 +0100

    create the student.h file

---
+++ include/student.h
@@ -0,0 +1,28 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+// include libraries
+#include <stdio.h>
+
+// define constants
+#define MAX_NAME_LENGTH 50
+#define MAX_TOPIC_LENGTH 100
+
+typedef struct{
+    int id;
+    char first_name[MAX_NAME_LENGTH];
+    char last_name[MAX_NAME_LENGTH];
+    char date_of_birth[100]; // format: DD-MM-YYYY
+    int semester;
+    char topic[MAX_TOPIC_LENGTH];
+}Student;
+
+// function declaration
+
+int add_student();
+int display_students();
+int search_student(int id);
+int update_student(int id);
+int delete_student(int id);
+
+#endif //STUDENT_H
